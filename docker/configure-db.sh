befdbd3d977280530c68b9c0225e86d88647f6c3
Rafael Mendes <rafael.mendes@zitrus.com.br>
2023-05-30 22:02:24 -0300

    adicionando exemplos configure-db.sh

--- docker/configure-db.sh
+++ docker/configure-db.sh
@@ -18,3 +18,4 @@ fi
 
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i criacao-banco.sql
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i criacao-funcoes.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i insercao-exemplos.sql
